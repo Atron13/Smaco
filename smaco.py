0a830f44c0ba2daec8e02874fa5a462d756b0611
Atron13 <vaibhav1322.13@gmail.com>
2020-08-23 21:34:07 +0530

    Load Module Added

--- smaco.py
+++ smaco.py
@@ -1,6 +1,8 @@
 import sys
+import time
 from PyQt5 import QtWidgets, uic
 from PyQt5 import QtCore,QtGui
+
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -12,50 +14,111 @@ class MainWindow(QtWidgets.QMainWindow):
         self.deactivate.clicked.connect(self.deactivating)
         self.numbercommands.clicked.connect(self.numberofcommands)
         self.loadcommands.clicked.connect(self.commands)
+        self.entervalue.clicked.connect(self.inputload)
         self.runeveryid.clicked.connect(self.run)
+        self.label_5.setText(">>>")
+        self.label_5.setAlignment(QtCore.Qt.AlignLeft)
+
+    def inputload(self):
+        finput=self.entervalueid.text()
+        self.datashowlabel.setText(">>> input file name is loaded")
+        self.label_5.setText(">>>")
+        self.label_5.setAlignment(QtCore.Qt.AlignLeft)
+        self.datashowlabel.setAlignment(QtCore.Qt.AlignLeft)
+        global inputfile
+        try:
+            inputfile=open(finput,'r')
+        except:
+            print("file not found")
+            self.datashowlabel.setText(">>> input file name is not loaded")
+            self.label_5.setText(">>>")
+            self.label_5.setAlignment(QtCore.Qt.AlignLeft)
+            self.datashowlabel.setAlignment(QtCore.Qt.AlignLeft)
+        else:
+            i=0
+            for each in inputfile:
+                infile[i]=each
+                #print(each)
+                i=i+1
+        
 
 
     def run(self):
+        intr=0
+        #for each in range(len(infile)):
+            #print(infile[each])
+        self.datashowlabel.setText(">>> run segment started in execution")
+        self.label_5.setText(">>>")
+        self.label_5.setAlignment(QtCore.Qt.AlignLeft)
+        self.datashowlabel.setAlignment(QtCore.Qt.AlignLeft)
+        number=self.numbercommndsid.value()
+        print(number)
         for i in range(number):
-            opc=(mem[i])//(10000)
-            op1 = (mem[i]%10000)//(1000-1)
-            op2 = (mem[i]%1000)
-            print(opc)
-            print(op1)
-            print(op2)
+            temp=int(mem[i])
+            opc=((temp)//(10000))
+            temp=int(mem[i])%10000
+            op1 =((temp)//(1000-1))
+            temp=int(mem[i])
+            op2 =((temp)%1000)
+            print("value of i")
+            print(i)
+            #print(opc)
+            if opc == 9:
+                mem[op2]=infile[intr]
+                print("value1")
+                print(infile[intr])
+                intr=intr+1
+                
+
 
 
     def commands(self):
         global mem
-        mem=[0]*number
+        global infile
+        number=self.numbercommndsid.value()
+        infile=[0]*10
+        mem=[0]*1000
         fname=self.loadcommandfileid.text()
-        self.datashowlabel.setText("file name is loaded")
-        self.datashowlabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.datashowlabel.setText(">>> command file name is loaded")
+        self.label_5.setText(">>>")
+        self.label_5.setAlignment(QtCore.Qt.AlignLeft)
+        self.datashowlabel.setAlignment(QtCore.Qt.AlignLeft)
         global file
         try:
             file=open(fname,'r')
         except:
             print("file not found")
-            self.datashowlabel.setText("file name is not loaded")
-            self.datashowlabel.setAlignment(QtCore.Qt.AlignCenter)
+            self.datashowlabel.setText(">>> command file name is not loaded")
+            self.label_5.setText(">>>")
+            self.label_5.setAlignment(QtCore.Qt.AlignLeft)
+            self.datashowlabel.setAlignment(QtCore.Qt.AlignLeft)
         else:
             #print("value of file")
             i=0
+            check=0
             for each in file:
                 mem[i]=each
-                print(each)
+                #print(each)
                 i=i+1
-
-            for i in range(len(mem)):
-                print(mem[i])
-
+                check=check+1
+            if check == number:
+                #for i in range(number):
+                print("yes same")
+            else:
+                print("number of commands are less than the file commands")
+                self.datashowlabel.setText(">>> number of commands are less than the file commands")
+                self.label_5.setText(">>>")
+                self.label_5.setAlignment(QtCore.Qt.AlignLeft)
+                self.datashowlabel.setAlignment(QtCore.Qt.AlignLeft)
 
 
     def numberofcommands(self):
     	global number
     	number=self.numbercommndsid.value()
-    	self.datashowlabel.setText("number of commands entered")
-    	self.datashowlabel.setAlignment(QtCore.Qt.AlignCenter)
+    	self.datashowlabel.setText(">>> number of commands entered")
+        #self.label_5.setText(">>>")
+        #self.label_5.setAlignment(QtCore.Qt.AlignLeft)
+        #self.datashowlabel.setAlignment(QtCore.Qt.AlignLeft)
 
     def activating(self):
     	self.labelforactivate.setText("Activate")
